f8c42972aaac4e923e8d5db67ad980098d462480
kaushal <kaushal_chakka@srmap.edu.in>
2026-06-13 14:00:31 +0530

    db connected!

--- app.js
+++ app.js
@@ -7,19 +7,8 @@ let client=new Client({
     host:process.env.DB_HOST,
     user: process.env.DB_USER,
     port:process.env.DB_PORT,
-    password: process.env.DB_PASSPORT,
+    password: process.env.DB_PASSWORD,
     database:process.env.DB_DATABASE
-});
-
-client.connect().then(()=>{
-    console.log('sql database connected');
-    return client.query('SELECT *');
-}).then(res=>{
-    console.log('Current Database Time:', res.rows[0].now);
-}).catch(err=>{
-    console.log('Connection error:',err.stack);
-}).finally(()=>{
-    client.end();
 });
 
 app.use(express.urlencoded({ extended:true }));
@@ -27,15 +16,58 @@ app.use(express.urlencoded({ extended:true }));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'));
 });
-
+let username,password,email;
 app.post('/login' , (req,res)=>{
-    let username=req.body.username;
-    let password=req.body.password;
-    let email=req.body.email;
-    console.log(username,password,email);
-    username, password,email    
+    username=req.body.username;
+    password=req.body.password;
+    email=req.body.email;
+    if(!search()){
+        dbtasks();
+        console.log(username,password,email);
+    }else{
+        console.log("You already have an account!!");
+    }
     res.send(`Thank you ${username}`);
 });
-
-
+async function dbtasks(){
+    try{
+        await client.connect();
+        console.log('Database connected!');
+        let createtable=`
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            username VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            password VARCHAR(100) NOT NULL
+        ); 
+        `;
+        await client.query(createtable);
+        console.log(`Users ready`);
+        let inertquery='INSERT INTO users(username,email,password) VALUES($1,$2,$3) RETURNING *;';
+        let insertvalues=[username,email,password];
+        console.log(insertvalues);
+        let result=await client.query(inertquery,insertvalues);
+        console.log('Succesfully added a user',result.rows[0]);
+    }catch(err){
+        console.error("error:",err.stack);
+    }finally{
+        await client.end();
+    }
+}
+async function search(){
+    try{
+        await client.connect();
+        let searchquery=`SELECT * FROM users WHERE email = $1;`;
+        let result=await client.query(searchquery,[email]);
+        if(result.rows.length>0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }catch(err){
+        console.error("error:",err.stack);
+    }finally{
+        await client.end();
+    }
+}
 app.listen(3000,()=>console.log('Server running in PORT:3000'));
